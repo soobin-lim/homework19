4e04569e352cb0f099a94e3930fba1554c7e4f20
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-12-13 08:19:23 +0900

    shoing index file but not api/transaction datas(missing)

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,10 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
-  "/manifest.webmanifest"
+  "/service-worker.js",
+  "/manifest.webmanifest",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
 ];
 
 // install
