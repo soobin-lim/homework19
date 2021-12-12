a4851bf984aca6f8ff7fef8511329eda05798232
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-12-13 08:21:25 +0900

    add bootstrap

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
   "/service-worker.js",
   "/manifest.webmanifest",
   "icons/icon-192x192.png",
-  "icons/icon-512x512.png"
+  "icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
 // install
