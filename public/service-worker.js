8ad92a45cc6bc59f7b61aca10581eb1220e866a1
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-12-13 08:24:54 +0900

    change 14-Stu_map style

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,8 +7,7 @@ const FILES_TO_CACHE = [
   "/service-worker.js",
   "/manifest.webmanifest",
   "icons/icon-192x192.png",
-  "icons/icon-512x512.png",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  "icons/icon-512x512.png"
 ];
 
 // install
